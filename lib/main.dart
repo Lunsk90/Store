3db2feb6785f51f22c28c2401cdf773b4b312db5
@inalcides <alcides.ramirez@bcr.gob.sv>
2024-01-10 10:37:58 -0600

    fix: fix podfile

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:fl_components/models/CartProvider.dart';
+import 'package:flutter/foundation.dart' show kIsWeb, defaultTargetPlatform;
 import 'package:fl_components/theme/app_theme.dart';
 import 'package:fl_components/router/app_routes.dart';
-import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inicializa Firebase con las opciones adecuadas para la plataforma actual
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyCcKeYgDqOi85l-1lNc9WkPhg47nIVBmjw',
-      appId: '1:599784223139:android:2915fed261de4b72562a16',
-      messagingSenderId: '599784223139',
-      projectId: 'products-65828',
-      storageBucket: 'gs://products-65828.appspot.com',
-      databaseURL: 'https://products-65828-default-rtdb.firebaseio.com/',
-    ),
-  );
+  // Verifica si la plataforma es Android y no es iOS, luego inicializa Firebase
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyCcKeYgDqOi85l-1lNc9WkPhg47nIVBmjw',
+        appId: '1:599784223139:android:2915fed261de4b72562a16',
+        messagingSenderId: '599784223139',
+        projectId: 'products-65828',
+        storageBucket: 'gs://products-65828.appspot.com',
+        databaseURL: 'https://products-65828-default-rtdb.firebaseio.com/',
+      ),
+    );
+  }
 
-  // Crea una instancia del proveedor del carrito
-  CartProvider cartProvider = CartProvider();
-
-  // Carga el historial de compras desde Firebase
-  await cartProvider.fetchPurchaseHistoryFromFirebase();
-
-  // Inicia la aplicación Flutter
-  runApp(
-    ChangeNotifierProvider.value(
-      value: cartProvider,
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
